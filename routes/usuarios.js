69d3e019fad77662a163868bccbbcd7d23c7b23e
Jose Miranda <vidaljose2004@gmail.com>
2023-02-02 22:40:51 -0400

    google

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,30 +1,33 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
-const {validarCampos} = require('../middlewares/validar-campos')
 const { usuariosGet,
     usuariosPut,
     usuariosPost,
     usuariosDelete,
     usuariosPatch } = require('../controllers/usuarios')
 const { esRoleValido, emailExiste, idExiste } = require('../helpers/db-validators');
+const {validarCampos,validarJWT,tieneRole} = require('../middlewares')
 
 const router = Router()
 
 router.get('/', usuariosGet)
-router.put('/:id',[
-    check('id','No es un id valido').isMongoId(),
+router.put('/:id', [
+    check('id', 'No es un id valido').isMongoId(),
     check('id').custom(idExiste),
     check('rol').custom(esRoleValido),
     validarCampos
-],usuariosPut)
-router.delete('/:id',[
-    check('id','No es un id valido').isMongoId(),
+], usuariosPut)
+router.delete('/:id', [
+    validarJWT,
+    // esAdminRole,
+    tieneRole('ADMIN_ROLE', 'VENTAS_ROLE'),
+    check('id', 'No es un id valido').isMongoId(),
     check('id').custom(idExiste),
     validarCampos
 ], usuariosDelete)
-router.post('/',[
+router.post('/', [
     check('nombre', 'EL nombre es obligatorio').not().isEmpty(),
-    check('password', 'EL password es obligatorio y debe tener mas de 6 letras').isLength({min:6}),
+    check('password', 'EL password es obligatorio y debe tener mas de 6 letras').isLength({ min: 6 }),
     check('correo', 'EL correo no es valido').isEmail(),
     check('correo').custom(emailExiste),
     check('rol').custom(esRoleValido),
